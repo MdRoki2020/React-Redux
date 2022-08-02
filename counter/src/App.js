b2f33e6dc7559da4d01808ca29da0b3ccfcd7c7b
MdRoki2020 <mroki815@gmail.com>
2022-08-02 11:32:17 +0600

    create couter app

---
+++ counter/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button className='btn btn-danger'>Counter</Button>
+    </div>
+  );
+}
+
+export default App;
